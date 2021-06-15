02d69d7a2bfd096b17ebe14e1e1b3e23bdae8c9a
srodzav <mrjchzz@gmail.com>
2021-06-14 20:53:06 -0500

    Cambios a menu .js y .php

---
+++ Views/js/menu.js
@@ -0,0 +1,5 @@
+var torta = document.getElementsById("m_torta");
+var taco = document.getElementsById("m_taco");
+var dorado = document.getElementsById("m_dorado");
+var gordita = document.getElementsById("m_gordita");
+
--- Views/php/menu.php
+++ Views/php/menu.php
@@ -19,36 +19,45 @@
     <hr style="width:100%;text-align:left;margin-left:0">
 
     <section id="our_menu" class="bcolor2">
-        <div class="single_menu">
+        <div class="single_menu bcolor3">
             <div class="menu_content">
                 <h4>Tortas Ahogadas <span>$55.00 c/u</span></h4>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, animi dolorum suscipit consequuntur reiciendis mollitia veritatis consectetur quae quia, quod sed corporis sapiente ex ipsam impedit dolor quis modi non!</p>
-                <button class="boton-carrito">Añadir</button>
+                <img src="../images/comida/Torta.jpg">
+                <br>
+                <button class="boton-carrito" id="m_torta">Añadir</button>
             </div>
         </div>
-        <div class="single_menu">
+        <div class="single_menu bcolor3">
             <div class="menu_content">
                 <h4>Tacos <span>$20.00 c/u</span></h4>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt aliquam alias culpa aliquid mollitia vero autem quia aspernatur? Ipsam tenetur, cumque omnis facilis excepturi magnam optio ipsum corporis itaque laudantium.</p>
-                <button class="boton-carrito">Añadir</button>
+                <img src="../images/comida/Taco_Marco.jpg">
+                <br>
+                <button class="boton-carrito" id="m_taco">Añadir</button>
             </div>
         </div>
-        <div class="single_menu">
+        <div class="single_menu bcolor3">
             <div class="menu_content">
                 <h4>Tacos Dorados  <span>$40.00 (5 pz)</span></h4>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum sit velit laborum doloremque adipisci? Corporis tempora, ipsum saepe placeat esse repudiandae est error porro ipsam recusandae nesciunt culpa ex doloribus!</p>
-                <button class="boton-carrito">Añadir</button>
+                <img src="../images/comida/Tacos.jpg">
+                <br>
+                <button class="boton-carrito" id="m_dorado">Añadir</button>
             </div>
         </div>
-        <div class="single_menu">
+        <div class="single_menu bcolor3">
             <div class="menu_content">
                 <h4>Gorditas  <span>$30.00 c/u</span></h4>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem eos exercitationem quaerat nostrum ab eius quam quas accusantium sequi? Aspernatur, repellendus? Tempore accusamus quas id repudiandae, sint atque asperiores facere?</p>
-                <button class="boton-carrito">Añadir</button>
+                <img src="../images/comida/Torta1.jpg">
+                <br>
+                <button class="boton-carrito"  id="m_gordita">Añadir</button>
             </div>
         </div>
 	</section>
 
     <script src="/PROYECTO/Views/js/app.js"></script>
+    <script src="/PROYECTO/Views/js/menu.js"></script>
 </body>
 </html>
