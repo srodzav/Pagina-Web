793515cb2f78f8b4b70c617213581743153d17de
srodzav <mrjchzz@gmail.com>
2021-06-16 11:41:29 -0500

    Cambios en los archivos js

--- Views/js/menu.js
+++ Views/js/menu.js
@@ -2,173 +2,39 @@ const carrito = document.getElementById("carrito");
 
 var cont = 0;
 
-function addCarrito(x){
+function addCarrito(nombre,precio,img,desc){
 
-    console.log(x);
+    console.log(nombre,precio,img,desc);
 
-    switch(x){
-        case 1: // TORTA
-            carrito.innerHTML +=
-            `
-            <div class="item" id="item_${cont}">
-                <div class="buttons">
-                    <button class="delete-btn" onclick="deleteItem('item_${cont}')"></button>
-                    <span class="like-btn is-active"></span>
-                </div>
-                <div class="image">
-                    <img src="../images/comida/Torta_128.jpg" alt="" />
-                </div>
-                <div class="description">
-                    <input type="text" name="nombre[]" id="nombre_${cont}" autocomplete="off" value="Torta" required>
-                    <span>Torta de maciza</span>
-                </div>
-                <div class="quantity">
-                    <button class="plus-btn" type="button" name="button" onclick="add(${cont});">
-                        <img src="../images/plus.svg" alt="" />
-                    </button>
+    carrito.innerHTML +=
+    `
+    <div class="item" id="item_${cont}">
+        <div class="buttons">
+            <button class="delete-btn" onclick="deleteItem('item_${cont}')"></button>
+            <span class="like-btn is-active"></span>
+        </div>
+        <div class="image">
+            <img class="image" src="../images/comida/${img}" alt="" />
+        </div>
+        <div class="description">
+            <input type="text" name="nombre[]" id="nombre_${cont}" autocomplete="off" value="${nombre}" required>
+            <span>${desc}</span>
+        </div>
+        <div class="quantity">
+            <button class="plus-btn" type="button" name="button" onclick="add(${cont});">
+                <img src="../images/plus.svg" alt="" />
+            </button>
 
-                    <input type="text" name="cantidad[]" id="cantidad_${cont}" autocomplete="off" value="1">
+            <input type="text" name="cantidad[]" id="cantidad_${cont}" autocomplete="off" value="1">
 
-                    <button class="minus-btn" type="button" name="button" onclick="sub(${cont});">
-                        <img src="../images/minus.svg" alt="" />
-                    </button>
-                </div>
-                <div class="total-price">$<input type="text" name="precio[]" id="precio_${cont}" value="55"></div>
-            </div>
-            `;
-            cont++;
-            break;
-
-        case 2: // TACO
-            carrito.innerHTML +=
-            `
-            <div class="item" id="item_${cont}">
-                <div class="buttons">
-                    <button class="delete-btn" onclick="deleteItem('item_${cont}')"></button>
-                    <span class="like-btn is-active"></span>
-                </div>
-                <div class="image">
-                    <img src="../images/comida/Taco_128.jpg" alt="" />
-                </div>
-                <div class="description">
-                    <input type="text" name="nombre[]" id="nombre_${cont}" autocomplete="off" value="Taco" required>
-                    <span>Taco de maciza</span>
-                </div>
-                <div class="quantity">
-                    <button class="plus-btn" type="button" name="button" onclick="add(${cont});">
-                        <img src="../images/plus.svg" alt="" />
-                    </button>
-
-                    <input type="text" name="cantidad[]" id="cantidad_${cont}" autocomplete="off" value="1">
-
-                    <button class="minus-btn" type="button" name="button" onclick="sub(${cont});">
-                        <img src="../images/minus.svg" alt="" />
-                    </button>
-                </div>
-                <div class="total-price">$<input type="text" name="precio[]" id="precio_${cont}" value="20"></div>
-            </div>
-            `;
-            cont++;
-            break;
-
-        case 3: // TACOS DORADOS
-            carrito.innerHTML += 
-            `
-            <div class="item"  id="item_${cont}">
-                <div class="buttons">
-                    <button class="delete-btn" onclick="deleteItem('item_${cont}')"></button>
-                    <span class="like-btn is-active"></span>
-                </div>
-                <div class="image">
-                    <img src="../images/comida/Tacos_128.jpg" alt="" />
-                </div>
-                <div class="description">
-                    <input type="text" name="nombre[]" id="nombre_${cont}" autocomplete="off" value="Tacos Dorados" required>
-                    <span>Orden de Tacos Dorados</span>
-                </div>
-                <div class="quantity">
-                    <button class="plus-btn" type="button" name="button" onclick="add(${cont});">
-                        <img src="../images/plus.svg" alt="" />
-                    </button>
-
-                    <input type="text" name="cantidad[]" id="cantidad_${cont}" autocomplete="off" value="1">
-
-                    <button class="minus-btn" type="button" name="button" onclick="sub(${cont});">
-                        <img src="../images/minus.svg" alt="" />
-                    </button>
-                        </div>
-                <div class="total-price">$<input type="text" name="precio[]" id="precio_${cont}" value="40"></div>
-            </div>
-            `;
-            cont++;
-            break;
-
-        case 4: // GORDITAS
-            carrito.innerHTML +=
-            `
-            <div class="item"  id="item_${cont}">
-                <div class="buttons">
-                    <button class="delete-btn" onclick="deleteItem('item_${cont}')"></button>
-                    <span class="like-btn  is-active"></span>
-                </div>
-                <div class="image">
-                    <img src="../images/comida/Gordita_128.jpg" alt="" />
-                </div>
-                <div class="description">
-                    <input type="text" name="nombre[]" id="nombre_${cont}" autocomplete="off" value="Gordita" required>
-                    <span>Gordita de maciza</span>
-                </div>
-                <div class="quantity">
-                    <button class="plus-btn" type="button" name="button" onclick="add(${cont});">
-                        <img src="../images/plus.svg" alt="" />
-                    </button>
-
-                    <input type="text" name="cantidad[]" id="cantidad_${cont}" autocomplete="off" value="1">
-
-                    <button class="minus-btn" type="button" name="button" onclick="sub(${cont});">
-                        <img src="../images/minus.svg" alt="" />
-                    </button>
-                        </div>
-                <div class="total-price">$<input type="text" name="precio[]" id="precio_${cont}" value="30"></div>
-            </div>
-            `;
-            cont++;
-            break;
-
-        case 5: // MICHELADAS
-            carrito.innerHTML +=
-            `
-            <div class="item"  id="item_${cont}">
-                <div class="buttons">
-                    <button class="delete-btn" onclick="deleteItem('item_${cont}')"></button>
-                    <span class="like-btn  is-active"></span>
-                </div>
-                <div class="image">
-                    <img src="../images/comida/Michelada_128.jpg" alt="" />
-                </div>
-                <div class="description">
-                    <input type="text" name="nombre[]" id="nombre_${cont}" autocomplete="off" value="Michelada" required>
-                    <span>Michelada</span>
-                </div>
-                <div class="quantity">
-                    <button class="plus-btn" type="button" name="button" onclick="add(${cont});">
-                        <img src="../images/plus.svg" alt="" />
-                    </button>
-
-                    <input type="text" name="cantidad[]" id="cantidad_${cont}" autocomplete="off" value="1">
-
-                    <button class="minus-btn" type="button" name="button" onclick="sub(${cont});">
-                        <img src="../images/minus.svg" alt="" />
-                    </button>
-                        </div>
-                <div class="total-price">$<input type="text" name="precio[]" id="precio_${cont}" value="39"></div>
-            </div>
-            `;
-            cont++;
-            break;
-            
-        default:
-    }
+            <button class="minus-btn" type="button" name="button" onclick="sub(${cont});">
+                <img src="../images/minus.svg" alt="" />
+            </button>
+        </div>
+        <div class="total-price">$<input type="text" name="precio[]" id="precio_${cont}" value="${precio}"></div>
+    </div>
+    `;
+    cont++;
 }
 
 function deleteItem(id){
@@ -179,52 +45,50 @@ function deleteItem(id){
 
 function add(id)
 {
-  var txtNumber = document.getElementById(`cantidad_${id}`); 
-  var newNumber = parseInt(txtNumber.value) + 1;
-  txtNumber.value = newNumber;
+    var txtNumber = document.getElementById(`cantidad_${id}`); 
+    var newNumber = parseInt(txtNumber.value) + 1;
+    txtNumber.value = newNumber;
 
-  var txtPrecio = document.getElementById(`precio_${id}`);
-  var newPrecio;
-  var tmp = document.getElementById(`nombre_${id}`);
+    var txtPrecio = document.getElementById(`precio_${id}`);
+    var newPrecio;
+    var tmp = document.getElementById(`nombre_${id}`);
 
-  switch(tmp.value){
-    case 'Torta':
-        newPrecio = parseInt(txtPrecio.value) +  55;
-        break;
-    case 'Taco':
-        newPrecio = parseInt(txtPrecio.value) +  20;
-        break;
-    case 'Tacos Dorados':
-        newPrecio = parseInt(txtPrecio.value) +  40;
-        break;
-    case 'Gordita':
-        newPrecio = parseInt(txtPrecio.value) +  30;
-        break;
-    case 'Michelada':
-        newPrecio = parseInt(txtPrecio.value) +  39;
-        break;
-    default:
-        break;
+    switch(tmp.value){
+        case 'Torta':
+            newPrecio = parseInt(txtPrecio.value) +  55;
+            break;
+        case 'Taco':
+            newPrecio = parseInt(txtPrecio.value) +  20;
+            break;
+        case 'Tacos Dorados':
+            newPrecio = parseInt(txtPrecio.value) +  40;
+            break;
+        case 'Gordita':
+            newPrecio = parseInt(txtPrecio.value) +  30;
+            break;
+        case 'Michelada':
+            newPrecio = parseInt(txtPrecio.value) +  39;
+            break;
+        default: break;
     }
-    
     txtPrecio.value = newPrecio;
 }
 
 function sub(id)
 {
-  var txtNumber = document.getElementById(`cantidad_${id}`);
-  var newNumber;
+    var txtNumber = document.getElementById(`cantidad_${id}`);
+    var newNumber;
     if(txtNumber.value > 0)
         newNumber = parseInt(txtNumber.value) - 1;
     else
         newNumber = 0
     txtNumber.value = newNumber;
 
-  var txtPrecio = document.getElementById(`precio_${id}`);
-  var newPrecio;
-  var tmp = document.getElementById(`nombre_${id}`);
-    
-  console.log(txtPrecio.value);
+    var txtPrecio = document.getElementById(`precio_${id}`);
+    var newPrecio;
+    var tmp = document.getElementById(`nombre_${id}`);
+
+    console.log(txtPrecio.value);
     if(txtPrecio.value > 0){
         switch(tmp.value){
         case 'Torta':
