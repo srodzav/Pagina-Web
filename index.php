1ce12c682182cc357d3f1506a8acf760de53d209
srodzav <mrjchzz@gmail.com>
2021-06-17 13:38:57 -0500

    PROYECTO TERMINADO 100%

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="Views/css/style.css">
+    <link rel="stylesheet" href="/PROYECTO/Views/css/style.css">
     <title>El Panal</title>
 </head>
 <body>
